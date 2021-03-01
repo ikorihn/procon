d69f29f147f0413a24c1a3e9da95bc7fd1ba1e9f
r57ty7 <16367098+r57ty7@users.noreply.github.com>
2021-03-01 12:59:35 +0900

    ABC138 Cまで15分くらい

---
+++ AtCoder/abc138_c.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+// DEBUG
+#ifdef _DEBUG
+#define debug(x) cout << #x << ": " << x << endl
+#else
+#define debug(x)
+#endif
+// iter
+#define REP(i, n) for (int i = 0; i < (int)(n); i++)
+#define REPD(i, n) for (int i = n - 1; i >= 0; i--)
+#define FOR(i, a, b) for (int i = a; i <= (int)(b); i++)
+#define FORD(i, a, b) for (int i = a; i >= (int)(b); i--)
+#define FORA(i, I) for (const auto& i : I)
+// vec
+#define ALL(x) x.begin(), x.end()
+#define SIZE(x) ((int)(x.size()))
+// 定数
+// 2.147483647×10^{9}:32bit整数のinf
+#define INF32 2147483647
+// 9.223372036854775807×10^{18}:64bit整数のinf
+#define INF64 9223372036854775807
+// 問題による
+#define MOD 1000000007
+
+int main() {
+  // 小数の桁数の出力指定
+  cout << fixed << setprecision(5);
+  // 入力の高速化用のコード
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int N;
+  cin >> N;
+
+  vector<int> A(N);
+  for (int i = 0; i < N; i++) {
+    cin >> A[i];
+  }
+
+  sort(ALL(A));
+
+  double ave = (double)(A[0] + A[1]) / 2;
+  FOR(i, 2, N - 1) {
+    double a = ave + (double)A[i];
+    ave = (double)a / 2;
+  }
+
+  cout << ave << endl;
+
+  return 0;
+}
